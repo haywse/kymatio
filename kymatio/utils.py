fddd702bbb0a8af738d83d38867157d6a95fb0c7
Joakim Andén <janden@flatironinstitute.org>
2020-02-04 11:39:58 -0500

    STY Clean up sklearn transformer docs

--- kymatio/utils.py
+++ kymatio/utils.py
@@ -4,23 +4,26 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class ScatteringTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, S, signal_shape, frontend='numpy'):
         """Creates an object that is compatible with the scikit-learn API
-        and implements the .transform method.
+        and implements the `.transform` method.
 
         Parameters
         ==========
 
         S: an instance of Scattering1D, Scattering2D or Scattering3D
-            This instance is called by the transformer
+            This instance is called by the transformer.
 
         signal_shape: tuple of ints
-            The shape of one sample. Scikit-learn convention is to work with
-            2D matrices only, of shape (n_samples, n_features). Data is
-            delivered in this way and has to be reshaped before transform
+            The shape of one sample. The `scikit-learn` convention is to work
+            with 2D arrays only of shape `(n_samples, n_features)`. Data is
+            delivered in this way and has to be reshaped before transforming.
+
+        frontend: string
+            The frontend of the `Scattering` object `S`.
 
         Output
         ======
-        Y: ndarray, shape (n_samples, n_scattering_features)
-            The scattering coefficients, raveled in C order
+        Y: ndarray of shape (n_samples, n_scattering_features)
+            The scattering coefficients, raveled in row-major order (C-like).
         """
 
         self.scattering = S
