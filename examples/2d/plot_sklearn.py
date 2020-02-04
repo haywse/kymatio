9901d35d866cb0ba7f1baee84c224cbc03f5f7e8
Joakim Andén <janden@flatironinstitute.org>
2020-02-04 11:34:10 -0500

    STY Clean up sklearn example

--- examples/2d/plot_sklearn.py
+++ examples/2d/plot_sklearn.py
@@ -1,6 +1,6 @@
 """
-ScikitLearn Transformer
-=====================================================================
+Scikit-learn transformer example
+================================
 Here we demonstrate a simple application of scattering as a transformer
 """
 
@@ -11,33 +11,35 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 
 # Use numpy or torch for the scattering
 frontend = 'numpy'
 
-
 # Create a scattering object (can be Scattering1D or Scattering3D as well)
-S = Scattering2D(shape = (8, 8), J = 1, frontend = frontend)
+S = Scattering2D(shape=(8, 8), J=1, frontend=frontend)
 
 # Use the toy digits dataset (8x8 digits)
 digits = datasets.load_digits()
 
 # Split data into train and test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    digits.images.reshape((len(digits.images), -1)), digits.target, test_size=0.5, shuffle=False)
+images = digits.images
+images = images.reshape((images.shape[0], -1))
+x_train, x_test, y_train, y_test = train_test_split(images, digits.target, test_size=0.5, shuffle=False)
 
-# Create the scikitlearn transformer
-st = ScatteringTransformer(S, digits.images[0].shape,frontend)
+# Create the scikit-learn transformer
+st = ScatteringTransformer(S, digits.images[0].shape, frontend)
 
 # Create a classifier: a support vector classifier
 classifier = LogisticRegression()
 estimators = [('scatter', st), ('clf', classifier)]
-pipe = Pipeline(estimators)
+pipeline = Pipeline(estimators)
 
 # We learn the digits on the first half of the digits
-pipe.fit(X_train, y_train)
+pipeline.fit(x_train, y_train)
 
 # Now predict the value of the digit on the second half:
-y_pred = pipe.predict(X_test)
-from sklearn.metrics import accuracy_score
+y_pred = pipeline.predict(x_test)
+
 print(accuracy_score(y_test, y_pred))
