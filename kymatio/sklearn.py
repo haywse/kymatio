341af941b2fe58cda5a81daba5ef2cb02cc46425
Joakim Andén <janden@flatironinstitute.org>
2020-02-10 09:38:48 -0500

    REF Move sklearn transformer into a mixin

--- kymatio/sklearn.py
+++ kymatio/sklearn.py
@@ -1,7 +1,24 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class ScatteringTransformer(BaseEstimator, TransformerMixin):
+class ScatteringTransformerMixin(BaseEstimator, TransformerMixin):
+    def fit(self, x=None, y=None):
+        # No fitting necessary.
+        return self
+
+    def predict(self, x):
+        x_reshaped = x.reshape((-1,) + self.S.shape)
+
+        transformed = self.S.scattering(x_reshaped)
+
+        Sx = transformed.reshape(x.shape[0], -1)
+
+        return Sx
+
+    transform = predict
+
+
+class ScatteringTransformer(ScatteringTransformerMixin):
     def __init__(self, S, signal_shape):
         """Creates an object that is compatible with the scikit-learn API
         and implements the `.transform` method.
@@ -23,20 +40,5 @@ class ScatteringTransformer(BaseEstimator, TransformerMixin):
             The scattering coefficients, raveled in row-major order (C-like).
         """
 
-        self.scattering = S
+        self.S = S
         self.signal_shape = signal_shape
-
-    def fit(self, X=None, y=None):
-        # no fitting necessary
-        return self
-
-    def predict(self, X):
-        X_reshaped = X.reshape((-1,) + self.signal_shape)
-
-        transformed = self.scattering(X_reshaped)
-
-        output = transformed.reshape(X.shape[0], -1)
-
-        return output
-
-    transform = predict
